bb1c4a9afaa8368e34d4dafebe854c54c08e7071
Sahil Ahmad <sahmad@EX2INDIA.LOCAL>
2025-06-26 12:13:17 +0530

    chore:style hero section

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -21,29 +21,36 @@ export default function Login() {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen w-full bg-gradient-to-br from-blue-50 to-blue-100 px-4">
-      <h1 className="text-3xl font-bold text-gray-800 mb-4 text-center">
-        Stay Updated on Disasters
-      </h1>
-      <p className="text-gray-600 mb-8 text-center">
-        Get the latest disaster news and alerts directly to your inbox.
-      </p>
-      <div className="flex justify-center items-center w-full max-w-md gap-2">
-        <Input
-          type="email"
-          placeholder="Enter your Name"
-          className="flex-1 rounded-md border border-gray-300 px-5 py-3 text-lg shadow-sm focus:outline-none focus:ring focus:ring-blue-300"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+    <div>
+      <div className="bg-[url(/disaster-hero-section.jpg)] bg-cover bg-center h-screen w-full">
+        <div className="flex flex-col justify-center h-screen w-full px-12 text-white">
+          <h1 className="text-6xl font-bold  mb-4">
+            <p>Stay Informed. Act Fast. </p>
+            <p>Save Lives.</p>
+          </h1>
+          <p className="w-[50%] mb-4">
+            Disaster Brodcaster is your real-time hub for reporting and tracking
+            natural calamities. Empower communities by sharing verified updates,
+            images, and eyewitness accounts to help authorities and citizens{" "}
+          </p>
+          <div className="flex justify-center items-center w-full max-w-md gap-2">
+            <Input
+              type="email"
+              placeholder="Enter your Name"
+              className="flex-1 px-5 py-3 text-lg bg-transparent text-white placeholder-white border border-white"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
 
-        <Button
-          type="submit"
-          className="px-6 py-3 text-lg text-white bg-green-600 hover:bg-green-700"
-          onClick={handleCheckIn}
-        >
-          Check In
-        </Button>
+            <Button
+              type="submit"
+              onClick={handleCheckIn}
+              className="px-6 py-3 text-lg text-white bg-white/10 backdrop-blur-md border border-white/20 rounded-md shadow-md hover:bg-white/20 transition"
+            >
+              Check In
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
